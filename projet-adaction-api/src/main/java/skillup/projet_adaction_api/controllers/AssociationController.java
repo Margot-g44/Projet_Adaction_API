01858e2c00261614866e2b43e119a43846fdf9e9
Werner Wolff <werner.wolff1109@gmail.com>
2025-10-07 15:28:06 +0200

    fix: endpoint wasn't properly made
    /api/assocations --> /api/associations

--- projet-adaction-api/src/main/java/skillup/projet_adaction_api/controllers/AssociationController.java
+++ projet-adaction-api/src/main/java/skillup/projet_adaction_api/controllers/AssociationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/assocations")
+@RequestMapping("/api/associations")
 public class AssociationController {
 
     private final AssociationService associationService;
