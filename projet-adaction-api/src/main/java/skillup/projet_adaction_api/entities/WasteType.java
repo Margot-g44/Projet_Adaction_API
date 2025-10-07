4cd676a7c4f291006ccb09765747aa0ef7095993
Margot Gouriou <margot.gouriou@gmail.com>
2025-10-07 15:34:53 +0200

    add: CRUD waste_types, create GET method

---
+++ projet-adaction-api/src/main/java/skillup/projet_adaction_api/entities/WasteType.java
@@ -0,0 +1,55 @@
+package skillup.projet_adaction_api.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "waste_types")
+public class WasteType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String value;
+
+    @Column(nullable = false)
+    private String label;
+
+    @Column(nullable = false)
+    private String classname;
+
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+}
+
